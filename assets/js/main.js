397a68716c563e27a098807d0ffeb89ae7b81488
Carissa Becerra <carissabecerra@Mac.attlocal.net>
2025-09-12 00:00:28 -0500

    Change to dark theme and font

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-console.log("Portfolio ready!");
-
-document.querySelectorAll("nav a[href^='#']").forEach(a => {
-  a.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute("href"))?.scrollIntoView({ behavior: "smooth" });
-  });
-});
